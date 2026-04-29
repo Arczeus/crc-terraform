2d5d67deb082e2e05b0366a13587f0a2fdbe876d
arczeus <slotalarek@gmail.com>
2026-04-29 20:52:52 +0200

    missing variables in variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,23 @@ variable "user_object_id" {
   type        = string
   description = "user object id"
 }
+
+variable "backend_resource_group_name" {
+  type        = string
+  description = "user object id"
+}
+
+variable "backend_storage_account_name" {
+  type        = string
+  description = "user object id"
+}
+
+variable "backend_container_name" {
+  type        = string
+  description = "user object id"
+}
+
+variable "backend_key" {
+  type        = string
+  description = "user object id"
+}
