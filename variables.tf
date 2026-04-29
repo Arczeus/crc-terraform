f8ab57de8d5c5eac713d6b51a7009b7e8d22db47
arczeus <slotalarek@gmail.com>
2026-04-29 21:20:45 +0200

    another test

--- variables.tf
+++ variables.tf
@@ -2,23 +2,3 @@ variable "user_object_id" {
   type        = string
   description = "user object id"
 }
-
-variable "resource_group_name" {
-  type        = string
-  description = "user object id"
-}
-
-variable "storage_account_name" {
-  type        = string
-  description = "user object id"
-}
-
-variable "container_name" {
-  type        = string
-  description = "user object id"
-}
-
-variable "key" {
-  type        = string
-  description = "user object id"
-}
